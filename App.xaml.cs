19dec2b734f589d08fe57377f1b2229b608844cd
Haya <hayachannel5@gmail.com>
2026-03-20 12:22:13 +0900

    init: Create Project

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace tool_type_instead_of_paste;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
